a5bb37740cded20bf12e53f86cc44f8110ad47b7
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-06-08 21:52:14 +0700

    Add circular progress chart

--- app/src/main/java/com/batikfy/batikfy/ui/game/quiz/QuizResultActivity.kt
+++ app/src/main/java/com/batikfy/batikfy/ui/game/quiz/QuizResultActivity.kt
@@ -1,12 +1,20 @@
 package com.batikfy.batikfy.ui.game.quiz
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.batikfy.batikfy.R
+import com.batikfy.batikfy.databinding.ActivityQuizResultBinding
 
 class QuizResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityQuizResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_result)
+        binding = ActivityQuizResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val percentage = 50
+        binding.progressResult.progress = percentage.toFloat()
     }
 }
