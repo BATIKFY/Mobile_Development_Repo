ec670e56afb00ef7cdd7293870a16c67d51d2f70
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-06-09 11:18:05 +0700

    Make view for profile setting

--- app/src/main/java/com/batikfy/batikfy/ui/profile/account/AccountActivity.kt
+++ app/src/main/java/com/batikfy/batikfy/ui/profile/account/AccountActivity.kt
@@ -1,12 +1,23 @@
 package com.batikfy.batikfy.ui.profile.account
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import com.batikfy.batikfy.R
 
 class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        onBack()
+    }
+
+    private fun onBack() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        })
     }
 }
