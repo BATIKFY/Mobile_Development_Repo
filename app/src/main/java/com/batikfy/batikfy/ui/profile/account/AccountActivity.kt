15bf36ba99e6f90d1c7949b42591165b88175837
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-06-09 10:56:53 +0700

    Make 2 view for profile

---
+++ app/src/main/java/com/batikfy/batikfy/ui/profile/account/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.batikfy.batikfy.ui.profile.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.batikfy.batikfy.R
+
+class AccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
---
+++ app/src/main/java/com/batikfy/batikfy/ui/profile/password/PasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.batikfy.batikfy.ui.profile.password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.batikfy.batikfy.R
+
+class PasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+    }
+}
