6caa9d6b3b9429f547d09efbb9b381fc054699e7
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-06-11 11:11:24 +0700

    Setting recycler view

--- app/src/main/java/com/batikfy/batikfy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/batikfy/batikfy/ui/home/HomeFragment.kt
@@ -9,18 +9,20 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.batikfy.batikfy.BuildConfig
 import com.batikfy.batikfy.MainActivity
 import com.batikfy.batikfy.R
+import com.batikfy.batikfy.data.remote.response.BatikItem
 import com.batikfy.batikfy.databinding.FragmentHomeBinding
+import com.batikfy.batikfy.utils.ViewModelFactory
 
-class HomeFragment : Fragment(), View.OnClickListener  {
+class HomeFragment : Fragment(), View.OnClickListener, GridBatikAdapter.OnItemClickCallback {
 
     private var _binding: FragmentHomeBinding? = null
-    private val homeViewModel by viewModels<HomeViewModel>()
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var gridBatikAdapter: GridBatikAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +34,30 @@ class HomeFragment : Fragment(), View.OnClickListener  {
         val root: View = binding.root
 
         binding.banner.setOnClickListener(this)
+
+        // Init RecyclerView and Adapter
+        gridBatikAdapter = GridBatikAdapter(listOf()) // Pass empty list initially
+        gridBatikAdapter.setOnItemClickCallback(this)
+
+        binding.rvHomeBatik.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = gridBatikAdapter
+        }
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(requireActivity(), BuildConfig.BASE_URL)
+        val homeViewModel: HomeViewModel by viewModels {
+            factory
+        }
+
+//        homeViewModel.getBatikItems().observe(viewLifecycleOwner) { batikItems ->
+//            gridBatikAdapter.setBatikItems(batikItems)
+//        }
     }
 
     override fun onDestroyView() {
@@ -47,5 +72,8 @@ class HomeFragment : Fragment(), View.OnClickListener  {
                 mainActivity?.navigateToScan()
             }
         }
+    }
+
+    override fun onItemClicked(data: BatikItem) {
     }
 }
