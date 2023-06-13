fba631f1997b4427c89eb0cc609dfaa37b41c049
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-06-13 07:47:28 +0700

    Login & register done. I'm want to rest

--- app/src/main/java/com/batikfy/batikfy/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/batikfy/batikfy/ui/auth/login/LoginActivity.kt
@@ -1,27 +1,27 @@
 package com.batikfy.batikfy.ui.auth.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.batikfy.batikfy.MainActivity
 import com.batikfy.batikfy.R
+import com.batikfy.batikfy.data.Result
+import com.batikfy.batikfy.data.remote.response.PostLoginResponse
 import com.batikfy.batikfy.databinding.ActivityLoginBinding
 import com.batikfy.batikfy.ui.auth.register.RegisterActivity
 import com.batikfy.batikfy.utils.AuthPreference
+import com.batikfy.batikfy.utils.ViewModelFactory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
-//        private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
-//        private val loginViewModel: LoginViewModel by viewModels {
-//            factory
-//        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,16 +31,17 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         // Remove Action Bar
         supportActionBar?.hide()
 
-//        binding.btnLogin.isEnabled = false
-//        binding.edPassword.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-//            override fun afterTextChanged(s: Editable?) {
-//                val passwordLength = s?.length ?: 0
-//                binding.btnLogin.isEnabled = (passwordLength >= 8)
-//                if(passwordLength < 8) binding.edPassword.error = getString(R.string.error_password)
-//            }
-//        })
+        binding.btnLogin.isEnabled = false
+        binding.edPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val passwordLength = s?.length ?: 0
+                binding.btnLogin.isEnabled = (passwordLength >= 8)
+                if (passwordLength < 8) binding.edPassword.error =
+                    getString(R.string.error_password)
+            }
+        })
         binding.signup.setOnClickListener(this)
         binding.btnLogin.setOnClickListener(this)
     }
@@ -54,44 +55,45 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btn_login -> {
                 var isValid = true
-//                val email = binding.edEmail.text.toString()
-//                val password = binding.edPassword.text.toString()
-//                if (email.isEmpty()) {
-//                    binding.edEmail.error = getString(R.string.error_email)
-//                    isValid = false
-//                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//                    binding.edEmail.error = getString(R.string.error_email_format)
-//                    isValid = false
-//                }
-//                if (password.isEmpty()) {
-//                    binding.edPassword.error = getString(R.string.error_password)
-//                    isValid = false
-//                }
+                val email = binding.edEmail.text.toString()
+                val password = binding.edPassword.text.toString()
+                if (email.isEmpty()) {
+                    binding.edEmail.error = getString(R.string.error_email)
+                    isValid = false
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.edEmail.error = getString(R.string.error_email_format)
+                    isValid = false
+                }
+                if (password.isEmpty()) {
+                    binding.edPassword.error = getString(R.string.error_password)
+                    isValid = false
+                }
                 if (isValid) {
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-//                    loginViewModel.login(email, password).observe(this) { result ->
-//                        if (result != null) {
-//                            when (result) {
-//                                is Result.Loading -> {
-//                                    showLoading(true)
-//                                }
-//                                is Result.Success -> {
-//                                    showLoading(false)
-//                                    checkLogin(result.data)
-//                                }
-//                                is Result.Error -> {
-//                                    showLoading(false)
-//                                    Toast.makeText(
-//                                        this,
-//                                        result.error,
-//                                        Toast.LENGTH_SHORT
-//                                    ).show()
-//                                }
-//                            }
-//                        }
-//                    }
+                    val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
+                    val loginViewModel: LoginViewModel by viewModels {
+                        factory
+                    }
+                    loginViewModel.login(email, password).observe(this) { result ->
+                        if (result != null) {
+                            when (result) {
+                                is Result.Loading -> {
+                                    showLoading(true)
+                                }
+                                is Result.Success -> {
+                                    showLoading(false)
+                                    checkLogin(result.data)
+                                }
+                                is Result.Error -> {
+                                    showLoading(false)
+                                    Toast.makeText(
+                                        this,
+                                        getString(R.string.error_login),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        }
+                    }
                 } else {
                     Toast.makeText(this, getString(R.string.error_login), Toast.LENGTH_SHORT)
                         .show()
@@ -100,25 +102,27 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-//    private fun checkLogin(data: LoginResponse) {
-//        if (data.error) {
-//            Toast.makeText(
-//                this,
-//                getString(R.string.error_login) + ": " + data.message,
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        } else {
-//            AuthPreference.initialize(this)
-//            AuthPreference.setToken(data.loginResult.token)
-//            showLoading(true)
-//            lifecycleScope.launch {
-//                delay(1000)
-//                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-//                startActivity(intent)
-//                finish()
-//            }
-//        }
-//    }
+    private fun checkLogin(data: PostLoginResponse) {
+        if (!data.success) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_login) + ": " + data.message,
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            if (data.data != null) {
+                AuthPreference.initialize(this)
+                AuthPreference.setToken(data.data.accessToken)
+                showLoading(true)
+                lifecycleScope.launch {
+                    delay(1000)
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
 
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
--- app/src/main/java/com/batikfy/batikfy/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/batikfy/batikfy/ui/auth/register/RegisterActivity.kt
@@ -8,19 +8,19 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.batikfy.batikfy.R
+import com.batikfy.batikfy.data.Result
+import com.batikfy.batikfy.data.remote.response.PostRegisterResponse
 import com.batikfy.batikfy.databinding.ActivityRegisterBinding
 import com.batikfy.batikfy.ui.auth.login.LoginActivity
+import com.batikfy.batikfy.utils.ViewModelFactory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityRegisterBinding
-//    private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
-//    private val registerViewModel: RegisterViewModel by viewModels {
-//        factory
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,16 +31,16 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
         onBack()
 
-//        binding.btnRegister.isEnabled = false
-//        binding.edPassword.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-//            override fun afterTextChanged(s: Editable?) {
-//                val passwordLength = s?.length ?: 0
-//                binding.btnRegister.isEnabled = (passwordLength >= 8)
-//                if(passwordLength < 8) binding.edPassword.error = getString(R.string.error_password)
-//            }
-//        })
+        binding.btnRegister.isEnabled = false
+        binding.edPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                val passwordLength = s?.length ?: 0
+                binding.btnRegister.isEnabled = (passwordLength >= 8)
+                if(passwordLength < 8) binding.edPassword.error = getString(R.string.error_password)
+            }
+        })
         binding.login.setOnClickListener(this)
         binding.btnRegister.setOnClickListener(this)
     }
@@ -54,49 +54,50 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btn_register -> {
                 var isValid = true
-//                val name = binding.edName.text.toString()
-//                val email = binding.edEmail.text.toString()
-//                val password = binding.edPassword.text.toString()
-//                if (name.isEmpty()) {
-//                    binding.edName.error = getString(R.string.error_name)
-//                    isValid = false
-//                }
-//                if (email.isEmpty()) {
-//                    binding.edEmail.error = getString(R.string.error_email)
-//                    isValid = false
-//                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-//                    binding.edEmail.error = getString(R.string.error_email_format)
-//                    isValid = false
-//                }
-//                if (password.isEmpty()) {
-//                    binding.edPassword.error = getString(R.string.error_password)
-//                    isValid = false
-//                }
+                val name = binding.edName.text.toString()
+                val email = binding.edEmail.text.toString()
+                val password = binding.edPassword.text.toString()
+                if (name.isEmpty()) {
+                    binding.edName.error = getString(R.string.error_name)
+                    isValid = false
+                }
+                if (email.isEmpty()) {
+                    binding.edEmail.error = getString(R.string.error_email)
+                    isValid = false
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.edEmail.error = getString(R.string.error_email_format)
+                    isValid = false
+                }
+                if (password.isEmpty()) {
+                    binding.edPassword.error = getString(R.string.error_password)
+                    isValid = false
+                }
                 if (isValid) {
-                    val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
-//                    registerViewModel.register(name, email, password).observe(this) { result ->
-//                        if (result != null) {
-//                            when (result) {
-//                                is Result.Loading -> {
-//                                    showLoading(true)
-//                                }
-//                                is Result.Success -> {
-//                                    showLoading(false)
-//                                    checkRegistration(result.data)
-//                                }
-//                                is Result.Error -> {
-//                                    showLoading(false)
-//                                    Toast.makeText(
-//                                        this,
-//                                        result.error,
-//                                        Toast.LENGTH_SHORT
-//                                    ).show()
-//                                }
-//                            }
-//                        }
-//                    }
+                    val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
+                    val registerViewModel: RegisterViewModel by viewModels {
+                        factory
+                    }
+                    registerViewModel.register(name, email, password).observe(this) { result ->
+                        if (result != null) {
+                            when (result) {
+                                is Result.Loading -> {
+                                    showLoading(true)
+                                }
+                                is Result.Success -> {
+                                    showLoading(false)
+                                    checkRegistration(result.data)
+                                }
+                                is Result.Error -> {
+                                    showLoading(false)
+                                    Toast.makeText(
+                                        this,
+                                        getString(R.string.error_register),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        }
+                    }
                 } else {
                     Toast.makeText(this, getString(R.string.error_register), Toast.LENGTH_SHORT)
                         .show()
@@ -105,23 +106,23 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-//    private fun checkRegistration(data: RegisterResponse) {
-//        if (data.error) {
-//            Toast.makeText(
-//                this,
-//                getString(R.string.error_register) + ": " + data.message,
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        } else {
-//            Toast.makeText(this, getString(R.string.success_register), Toast.LENGTH_SHORT).show()
-//            showLoading(true)
-//            lifecycleScope.launch {
-//                delay(1000)
-//                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
-//    }
+    private fun checkRegistration(data: PostRegisterResponse) {
+        if (data.succses == "false") {
+            Toast.makeText(
+                this,
+                getString(R.string.error_register) + ": " + data.message,
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(this, getString(R.string.success_register), Toast.LENGTH_SHORT).show()
+            showLoading(true)
+            lifecycleScope.launch {
+                delay(1000)
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
